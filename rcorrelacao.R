22a9ed38716c4623aa91e8c23cc78a598bbd94c8
Ricardo Avancini <avancini.rf@gmail.com>
2018-03-06 21:14:38 +0000

    iniciando repositorio

---
+++ rcorrelacao.R
@@ -0,0 +1,8 @@
+
+### ---------------------------------------------------------------------------
+### Título: Função para Calcular Correlação de planilhas usando R
+### Autor: Ricardo Avancini
+### e-mail: avancini.rf@gmail.com
+### Repositório: https://github.com/avancinirf/rcorrelacao
+### ---------------------------------------------------------------------------
+
